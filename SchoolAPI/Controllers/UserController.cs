9d6c4859c4c11fda15d99fc16865c0c7eec6f3b1
sderosa <C0astal89>
2020-07-03 19:16:27 -0400

    Feature: Added files for module 7 - Handling Get Requests

---
+++ SchoolAPI/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using Contracts;
+using Entities.DataTransferObjects;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace SchoolAPI.Controllers
+{
+    [Route("api/v1/user")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IRepositoryManager _repository;
+        private readonly ILoggerManager _logger;
+        //private readonly IMapper _mapper;
+
+        public UserController(IRepositoryManager repository, ILoggerManager logger)
+        {
+            _repository = repository;
+            _logger = logger;
+            //_mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            try
+            {
+                var users = _repository.User.GetAllUsers(trackChanges: false);
+
+                //var companiesDto = _mapper.Map<IEnumerable<UserDto>>(companies);
+
+                return Ok(users);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong in the {nameof(GetUsers)} action {ex}");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+    }
+}
---
+++ SchoolAPI/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Entities.DataTransferObjects;
+using Entities.Models;
+
+namespace SchoolAPI
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, UserDto>()
+                    .ForMember(c => c.UserName,
+                        opt => opt.MapFrom(x => string.Join(' ', x.Status, x.SystemRoleID)));
+        }
+    }
+}
