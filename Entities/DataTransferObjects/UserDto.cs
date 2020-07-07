79a66a509f4b0ce51dfe88776033ab16faf0fab1
sderosa <C0astal89>
2020-07-06 21:38:20 -0400

    Feature: Module 8 (POST, PUT, PATCH, DELETE) and Model Validation

--- Entities/DataTransferObjects/UserDto.cs
+++ Entities/DataTransferObjects/UserDto.cs
@@ -6,6 +6,7 @@ namespace Entities.DataTransferObjects
 {
     public class UserDto
     {
+        public Guid id { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
